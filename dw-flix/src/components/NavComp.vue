c0d16588ac6055e7c12efa953ac7140627ec9c10
luizatorrens <luiza.torrens.2006@gmail.com>
2022-09-21 09:56:03 -0300

    ook

--- dw-flix/src/components/NavComp.vue
+++ dw-flix/src/components/NavComp.vue
@@ -5,45 +5,16 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item p-1">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <RouterLink to="/" class="nav-link links">Home</RouterLink>
           </li>
-          <li class="nav-item dropdown p-1">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdownMenuLink"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Filmes
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="#">Ação</a></li>
-              <li><a class="dropdown-item" href="#">Romance</a></li>
-              <li><a class="dropdown-item" href="#">Comédia</a></li>
-            </ul>
+          <li class="nav-item p-1">
+            <RouterLink to="/filmes" class="nav-link links">Filmes</RouterLink>
           </li>
-          <li class="nav-item dropdown p-1">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdownMenuLink"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Séries
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="#">Ação</a></li>
-              <li><a class="dropdown-item" href="#">Romance</a></li>
-              <li><a class="dropdown-item" href="#">Comédia</a></li>
-            </ul>
+          <li class="nav-item p-1">
+            <RouterLink to="/series" class="nav-link links">Séries</RouterLink>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
-
--- dw-flix/src/router/index.js
+++ dw-flix/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import FilmesView from "../views/FilmesView.vue";
+import SeriesView from "../views/SeriesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/filmes",
+      name: "filmes",
+      component: FilmesView,
+    },
+    {
+      path: "/series",
+      name: "series",
+      component: SeriesView,
     },
   ],
 });
