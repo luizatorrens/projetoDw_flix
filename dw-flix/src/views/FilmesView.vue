4ee3b8e510367bb7e2c6835715dc04cabaae1a65
= <=>
2022-09-26 14:30:27 -0300

    ok

--- dw-flix/src/views/FilmesView.vue
+++ dw-flix/src/views/FilmesView.vue
@@ -1,1 +1,69 @@
-<template>aaaa</template>
+<script>
+import CardMovie from "../components/filmes/CardMovie.vue";
+
+export default {
+  components: { CardMovie },
+  data() {
+    return {
+      movies: [
+        {
+          id: 1,
+          imagem: new URL("../assets/img/enrolados.jpg", import.meta.url).href,
+          autor: "Fulano",
+        },
+        {
+          id: 2,
+          imagem: new URL("../assets/img/Ameninalivros.jpg", import.meta.url)
+            .href,
+          autor: "Beltrano",
+        },
+        {
+          id: 3,
+          imagem: new URL("../assets/img/ACABANA.jpg", import.meta.url).href,
+        },
+        {
+          id: 4,
+          imagem: new URL("../assets/img/enrolados.jpg", import.meta.url).href,
+        },
+        {
+          id: 5,
+          imagem: new URL("../assets/img/Ameninalivros.jpg", import.meta.url)
+            .href,
+        },
+        {
+          id: 6,
+          imagem: new URL("../assets/img/ACABANA.jpg", import.meta.url).href,
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <h2 class="fs-1 text-light text-center p-2">Comédia</h2>
+  <div class="card-group card-filmes">
+    <CardMovie v-for="movie of movies" :key="movie.id" :filme="movie" />
+  </div>
+  <h2 class="fs-1 text-light text-center p-2">Suspense</h2>
+  <div class="card-group card-filmes">
+    <CardMovie v-for="movie of movies" :key="movie.id" :filme="movie" />
+  </div>
+  <h2 class="fs-1 text-light text-center p-2">Romance</h2>
+  <div class="card-group card-filmes">
+    <CardMovie v-for="movie of movies" :key="movie.id" :filme="movie" />
+  </div>
+  
+</template>
+
+<style scooped>
+.card-filmes {
+  background-color: black;
+}
+
+.card {
+  background-color: black;
+}
+h2 {
+  background-color: black;
+}
+</style>
---
+++ dw-flix/src/views/SeriesView.vue
@@ -0,0 +1,64 @@
+<script>
+import CardSerie from "../components/séries/CardSerie.vue";
+
+export default {
+  components: { CardSerie },
+  data() {
+    return {
+      series: [
+        {
+          id: 7,
+          imagem: new URL("../assets/img/enrolados.jpg", import.meta.url).href,
+          autor: "Fulano",
+        },
+        {
+          id: 8,
+          imagem: new URL("../assets/img/Ameninalivros.jpg", import.meta.url)
+            .href,
+          autor: "Beltrano",
+        },
+        {
+          id: 9,
+          imagem: new URL("../assets/img/ACABANA.jpg", import.meta.url).href,
+        },
+        {
+          id: 10,
+          imagem: new URL("../assets/img/enrolados.jpg", import.meta.url).href,
+        },
+        {
+          id: 11,
+          imagem: new URL("../assets/img/Ameninalivros.jpg", import.meta.url)
+            .href,
+        },
+        {
+          id: 12,
+          imagem: new URL("../assets/img/ACABANA.jpg", import.meta.url).href,
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <h2 class="fs-1 text-light text-center p-2">Romance</h2>
+  <div class="card-group card-filmes">
+    <CardSerie v-for="serie of series" :key="serie.id" :serie="serie" />
+  </div>
+  <h2 class="fs-1 text-light text-center p-2">Comédia</h2>
+  <div class="card-group card-filmes">
+    <CardSerie v-for="serie of series" :key="serie.id" :serie="serie" />
+  </div>
+</template>
+
+<style scooped>
+.card-filmes {
+  background-color: black;
+}
+
+.card {
+  background-color: black;
+}
+h2 {
+  background-color: black;
+}
+</style>
